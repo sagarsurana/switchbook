2b1bcbad0a340207fb8e05e6ec10177d1d0ecf70
Aviral Sharma <sharma22@cs.washington.edu>
2018-12-11 01:21:20 -0800

    notification comment last line

--- switchBook/ProfileViewController.swift
+++ switchBook/ProfileViewController.swift
@@ -92,67 +92,7 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
             
 //            ref.child("users").child("user_information").setValue(["List": userInput.text])
 
-         // API NOT WORKING!
-            
-//            let m  = userInput.text?.replacingOccurrences(of: " ", with: "")
-//            let url = URL(string: "https://www.googleapis.com/books/v1/volumes?q=" + m!)!
-//            let urlRequest = URLRequest(url: url)
-//
-//            // set up the session
-//            let config = URLSessionConfiguration.default
-//            let session = URLSession(configuration: config)
-//
-//            // make the request
-//            let task = session.dataTask(with: urlRequest) {
-//                (data, response, error) in
-//                // check for any errors
-//                guard error == nil else {
-//                    let alert = UIAlertController(title: "Incorrect Details", message: "No such book found", preferredStyle: UIAlertController.Style.alert)
-//
-//                    // add an action (button)
-//                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-//
-//                    // show the alert
-//                    self.present(alert, animated: true, completion: nil)
-//                    return;
-//                }
-//                // make sure we got data
-//                guard let responseData = data else {
-//                    let alert = UIAlertController(title: "Fatal Error", message: "Bad Getaway", preferredStyle: UIAlertController.Style.alert)
-//
-//                    // add an action (button)
-//                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-//
-//                    // show the alert
-//                    self.present(alert, animated: true, completion: nil)
-//                    return;
-//                }
-//                do {
-//                    guard let todo = try JSONSerialization.jsonObject(with: responseData, options: [])
-//                        as? [String: Any] else {
-//                            let alert = UIAlertController(title: "Incorrect Details", message: "Couldn't convert data", preferredStyle: UIAlertController.Style.alert)
-//
-//                            // add an action (button)
-//                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-//
-//                            // show the alert
-//                            self.present(alert, animated: true, completion: nil)
-//                            return;
-//
-//                    }
-//                } catch  {
-//                    let alert = UIAlertController(title: "Incorrect Details", message: "Couldn't convert data", preferredStyle: UIAlertController.Style.alert)
-//
-//                    // add an action (button)
-//                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-//
-//                    // show the alert
-//                    self.present(alert, animated: true, completion: nil)
-//                    return;
-//                }
-//            }
-//            task.resume()
-
+        
         
     }
 }
